7310137fd9f6bfaf40e089a773a02f797930572a
Jeron <35764802+Jeron@users.noreply.github.com>
2018-01-29 14:31:55 +0530

    vow rvar.c

---
+++ vow or var.c
@@ -0,0 +1,13 @@
+#include<stdo.h>
+#include<conio.h>
+void main()
+{
+char ch;
+printf("Enter the character:");
+scanf("%c",&ch);
+if(ch=='a',ch=='e',ch=='i',ch=='o',ch=='u',ch='A',ch=='E',ch=='I',ch=='O',ch=='U')
+printf("%c is vowel",ch);
+else
+printf("%c is variable",ch);
+getch();
+}
