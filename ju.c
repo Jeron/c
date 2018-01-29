6ce98aa978286ab4fd2607cb49746b3a54ce4b1d
Jeron <35764802+Jeron@users.noreply.github.com>
2018-01-29 13:45:41 +0530

    check the given number is positive or negative

---
+++ ju.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<coni.h>
+void main()
+{
+int i;
+printf("Enter the number :");
+scanf("%d",&i);
+if(i<o)
+printf("%d is negative number",i);
+else if(i>0)
+printf("%d is positive number",i);
+else
+printf("The number is neither positive nor negative");
+getch();
