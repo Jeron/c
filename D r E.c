db2d9468de8f7415bf61bbe65dba0b48326b823f
Jeron <35764802+Jeron@users.noreply.github.com>
2018-01-29 13:56:15 +0530

    D r E.c

---
+++ D r E.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int i;
+printf("Enter the number:");
+scanf("%d",&i);
+if(i%2==0)
+printf("%d is even number",i);
+else
+printf("%d s odd number",i);
+getch();
+}
