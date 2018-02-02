d6f1680b630d6871bd874b8fb1b75a13899ce999
Jeron <35764802+Jeron@users.noreply.github.com>
2018-02-02 16:12:33 +0530

    jer8.c

---
+++ jer8.c
@@ -0,0 +1,13 @@
+#include<Stdio.h>
+#include<conio.h>
+void main()
+{
+int i,j,k;
+printf("Enter the value of i:");
+scanf("%d",&i);
+printf("Enter the value of j:");
+scanf("%d",&j);
+k=i^j;
+printf("The value is%d",&k);
+getch();
+}
