a98e8b9f34e561e067736747ca99d5c1dfca51bd
Jeron <35764802+Jeron@users.noreply.github.com>
2018-02-12 13:59:05 +0530

    Update jer29.c

--- jer29.c
+++ jer29.c
@@ -2,24 +2,19 @@
 #include<conio.h>
 void main()
 {
-  int countch=0;
-  int countwd=1;
-
-  printf("Enter your sentence in lowercase: ");
-  char ch='a';
-  while(ch!='\r')
-  {
-    ch=getche();
-    if(ch==' ')
-      countwd++;
-    else
-      countch++;
-  }
-
-  printf("\n Words = ",countwd);
-
-  printf("Characters = ",countch-1);
-
-  getch();
-
+int countch=0;
+int countwd=1;
+printf("Enter your sentence in lowercase: ");
+char ch='a';
+while(ch!='\r')
+{
+ch=getche();
+if(ch==' ')
+countwd++;
+else
+countch++;
+}
+printf("\n Words = ",countwd);
+printf("Characters = ",countch-1);
+getch();
 }
