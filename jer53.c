8747bceb2940bc59d42306c60b227d1f7bc00d09
Jeron <35764802+Jeron@users.noreply.github.com>
2018-02-23 15:59:07 +0530

    jer53.c

---
+++ jer53.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+int n,rem,rem1,sum;
+clrscr();
+printf("\n enter the number:");
+scanf("%d",&n);
+rem=n%10;
+rem1=n/10;
+sum=rem+rem1;
+printf("/n sum is:%d",sum);
+return 0;
+}
