2048aaf15fee38c60566e0a8dd00087b7687655c
Jeron <35764802+Jeron@users.noreply.github.com>
2018-03-09 15:59:01 +0530

    jer81.c

---
+++ jer81.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include<conio.h>
+void main(void) 
+{
+int kabali,opponent,i;
+for(i=1;i<=3;i++)
+{
+scanf("%d %d\n",&kabali,&opponent);
+if(kabali>opponent)
+{
+printf("\n%d",kabali-opponent);
+}
+else
+{
+printf("\n%d",opponent-kabali);
+}
+}
+getch();
+}
