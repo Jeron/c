0a53aadf44f32cb9e388f6cfbcce5ce229a8d903
Jeron <35764802+Jeron@users.noreply.github.com>
2018-03-09 15:51:01 +0530

    jer79.c

---
+++ jer79.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include<conio.h>
+void main(void) 
+{
+int n,m,prod,i,p,flag=0;
+scanf("%d %d",&n,&m);
+prod=m*n;
+for(i=1;i<=prod/2;i++)
+{
+p=i*i;
+if(p==prod)
+{
+printf("yes");
+flag=1;
+break;
+}
+}
+if(flag==0)
+printf("no");
+getch();
+}
